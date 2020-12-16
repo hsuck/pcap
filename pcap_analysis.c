790e3e62fcac2de1268dc44ad633d86abb0492da
hsuck <xmjj726716@gmail.com>
2020-12-16 22:48:48 +0800

    v 2.0

--- pcap_analysis.c
+++ pcap_analysis.c
@@ -8,23 +8,48 @@
 #include <netinet/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <netinet/ip_icmp.h>
 
 #define MAC_ADDR_LEN 18
 
 void dump_ethernet( u_int32_t len, const u_char *content );
-void dump_ip( u_int32_t len, const u_char *content );
-void dump_tcp( u_int32_t len, const u_char *content );
-void dump_udp( u_int32_t len, const u_char *content );
+void dump_ip( struct ip *ip );
+void dump_tcp( struct tcphdr *tcp );
+void dump_tcp_mini( struct tcphdr *tcp );
+void dump_udp( struct udphdr *udp );
+void dump_icmp( struct icmp *icmp );
 void pcap_callback( u_char* arg, const struct pcap_pkthdr *header, const u_char *content );
 char *mac_transfer( u_char *b );
 char *ttoa( u_int8_t flag );
 char *ftoa( u_int16_t flag );
 
-
+char *type_name[] = {
+    "Echo Reply",               /* Type  0 */
+    "Undefine",                 /* Type  1 */
+    "Undefine",                 /* Type  2 */
+    "Destination Unreachable",  /* Type  3 */
+    "Source Quench",            /* Type  4 */
+    "Redirect (change route)",  /* Type  5 */
+    "Undefine",                 /* Type  6 */
+    "Undefine",                 /* Type  7 */
+    "Echo Request",             /* Type  8 */
+    "Undefine",                 /* Type  9 */
+    "Undefine",                 /* Type 10 */
+    "Time Exceeded",            /* Type 11 */
+    "Parameter Problem",        /* Type 12 */
+    "Timestamp Request",        /* Type 13 */
+    "Timestamp Reply",          /* Type 14 */
+    "Information Request",      /* Type 15 */
+    "Information Reply",        /* Type 16 */
+    "Address Mask Request",     /* Type 17 */
+    "Address Mask Reply",       /* Type 18 */
+    "Unknown"                   /* Type 19 */
+};
 
 int main( int argc, char **argv ){
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *handle;
+    pcap_t *handle = NULL;
+    pcap_dumper_t *dumper = NULL;
     
     if( argc < 2 ){
         char *device = "enp0s3";
@@ -39,12 +64,14 @@ int main( int argc, char **argv ){
     }
     else{
         int num = 0;
-        char *filename = NULL;
+        char *filename = NULL, *dumpfile = NULL;
         for( int i = 1; i < argc; i++ ){
             if( !strcmp( argv[i], "-n" ) && argv[i + 1] != NULL )
                 num = atoi( argv[i + 1] );
             if( !strcmp( argv[i], "-f" ) && argv[i + 1] != NULL )
                 filename = argv[i + 1];
+            if( !strcmp( argv[i], "-o" ) && argv[i + 1] != NULL )
+                dumpfile = argv[i + 1];
         }
         
         if( num != 0 && filename != NULL ){
@@ -55,7 +82,18 @@ int main( int argc, char **argv ){
                 exit(1);
             }
 
-            pcap_loop( handle, num, pcap_callback, NULL );
+            if( dumpfile != NULL ){
+                dumper = pcap_dump_open( handle, dumpfile );
+                if( !dumper ){
+                    fprintf( stderr, "pcap_dump_open: %s\n", pcap_geterr( handle ) );
+                    pcap_close( handle );
+                    exit(1);
+                }
+                
+                pcap_loop( handle, num, pcap_callback, (u_char *)dumper );
+            }
+            else
+                pcap_loop( handle, num, pcap_callback, NULL );
         }
         else if( num == 0 && filename != NULL ){
             handle = pcap_open_offline( filename, errbuf );
@@ -64,8 +102,19 @@ int main( int argc, char **argv ){
                 fprintf( stderr, "pcap_open_offline: %s\n", errbuf );
                 exit(1);
             }
-
-            pcap_loop( handle, -1, pcap_callback, NULL );
+            
+            if( dumpfile != NULL ){
+                dumper = pcap_dump_open( handle, dumpfile );
+                if( !dumper ){
+                    fprintf( stderr, "pcap_dump_open: %s\n", pcap_geterr( handle ) );
+                    pcap_close( handle );
+                    exit(1);
+                }
+                
+                pcap_loop( handle, num, pcap_callback, (u_char *)dumper );
+            }
+            else
+                pcap_loop( handle, num, pcap_callback, NULL );
         }    
         else if( num != 0 && filename == NULL ){
             char *device = "enp0s3";
@@ -76,7 +125,18 @@ int main( int argc, char **argv ){
                 exit(1);
             }
 
-            pcap_loop( handle, num, pcap_callback, NULL );
+            if( dumpfile != NULL ){
+                dumper = pcap_dump_open( handle, dumpfile );
+                if( !dumper ){
+                    fprintf( stderr, "pcap_dump_open: %s\n", pcap_geterr( handle ) );
+                    pcap_close( handle );
+                    exit(1);
+                }
+                
+                pcap_loop( handle, num, pcap_callback, (u_char *)dumper );
+            }
+            else
+                pcap_loop( handle, num, pcap_callback, NULL );
         }   
         else{
             fprintf( stderr, "Usage: ./pcap_analysis [option]... [filename]...\n" );
@@ -86,6 +146,10 @@ int main( int argc, char **argv ){
         } 
     }
     
+    if( dumper != NULL ){
+        pcap_dump_flush( dumper );
+        pcap_dump_close( dumper );
+    }
     pcap_close( handle );
 }
 
@@ -99,6 +163,12 @@ char *mac_transfer( u_char *b ){
 }
 
 void pcap_callback( u_char* arg, const struct pcap_pkthdr *header, const u_char *content ){
+    pcap_dumper_t *dumper = (pcap_dumper_t *)arg;
+    if( dumper != NULL ){
+        pcap_dump( arg, header, content );
+        pcap_dump_flush( dumper );
+    }
+
     static int num = 0;
     struct tm *local = localtime( &header->ts.tv_sec );
     char timestr[16];
@@ -152,7 +222,7 @@ void dump_ethernet( u_int32_t len, const u_char *content ){
             break;
         
         case ETHERTYPE_IP:
-            dump_ip( len, content );
+            dump_ip( (struct ip *)( content + ETHER_HDR_LEN ) );
             break; 
         
         case ETHERTYPE_REVARP:
@@ -206,8 +276,8 @@ char *ip_ftoa( u_int16_t flag ){
     return str;
 }
 
-void dump_ip( u_int32_t len, const u_char *content ){
-    struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
+void dump_ip( struct ip *ip ){
+    /*struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );*/
     u_int version = ip->ip_v;
     u_int hdr_len = ip->ip_hl << 2;
     u_char tos = ip->ip_tos;
@@ -234,17 +304,18 @@ void dump_ip( u_int32_t len, const u_char *content ){
     printf( "\t| Destination IP Address:            %15s|\n", inet_ntoa( ip->ip_dst ) );
     printf("\t+---------------------------------------------------+\n");
 
+    char *p = (char *)ip + ( ip->ip_hl << 2 );
     switch( protocol ){
         case IPPROTO_UDP:
-            dump_udp( len, content );
+            dump_udp( (struct udphdr *)p );
             break;
 
         case IPPROTO_TCP:
-            dump_tcp( len, content );
+            dump_tcp( (struct tcphdr *)p );
             break;
 
         case IPPROTO_ICMP:
-            printf("Next is ICMP\n");
+            dump_icmp( (struct icmp *)p );
             break;
 
         default:
@@ -253,9 +324,9 @@ void dump_ip( u_int32_t len, const u_char *content ){
     }
 }
 
-void dump_tcp( u_int32_t len, const u_char *content ){
-    struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
-    struct tcphdr *tcp = (struct tcphdr *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );
+void dump_tcp( struct tcphdr *tcp ){
+    /*struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
+    struct tcphdr *tcp = (struct tcphdr *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );*/
 
     u_int16_t sport = ntohs( tcp->source );
     u_int16_t dport = ntohs( tcp->dest );
@@ -291,9 +362,25 @@ void dump_tcp( u_int32_t len, const u_char *content ){
     printf("\t+-------------------------+-------------------------+\n");
 }
 
-void dump_udp( u_int32_t len, const u_char *content ){
-    struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
-    struct udphdr *udp = (struct udphdr *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );
+void dump_tcp_mini( struct tcphdr *tcp ){
+    /*struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
+    struct tcphdr *tcp = (struct tcphdr *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );*/
+
+    u_int16_t sport = ntohs( tcp->source );
+    u_int16_t dport = ntohs( tcp->dest );
+    u_int32_t seq = ntohl( tcp->seq );
+    
+    printf("\n\tProtocol: TCP\n");
+    printf("\t+-------------------------+-------------------------+\n");
+    printf( "\t| Source Port:       %5u| Destination Port:  %5u|\n", sport, dport );
+    printf("\t+-------------------------+-------------------------+\n");
+    printf( "\t| Sequence Number:                        %10u|\n", seq );
+    printf("\t+---------------------------------------------------+\n");
+}
+
+void dump_udp( struct udphdr *udp ){
+    /*struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
+    struct udphdr *udp = (struct udphdr *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );*/
 
     u_int16_t sport = ntohs( udp->source );
     u_int16_t dport = ntohs( udp->dest );
@@ -308,3 +395,61 @@ void dump_udp( u_int32_t len, const u_char *content ){
     printf("\t+-------------------------+-------------------------+\n");
 }
 
+void dump_icmp( struct icmp *icmp ){
+    /*struct ip *ip = (struct ip *)( content + ETHER_HDR_LEN );
+    struct icmp *icmp = (struct icmp *)( content + ETHER_HDR_LEN + ( ip->ip_hl << 2 ) );*/
+
+    u_int8_t type = icmp->icmp_type;
+    u_int8_t code = icmp->icmp_code;
+    u_int16_t checksum = ntohs( icmp->icmp_cksum );
+    
+    if( type < 0 || 20 <= type )
+        type -= 1;
+
+    printf( "\n\tProtocol: ICMP (%s)\n", type_name[type] );
+
+    printf("\t+------------+------------+-------------------------+\n");
+    printf( "\t| Type:   %3u| Code:   %3u| Checksum:          %5u|\n", type, code, checksum );
+    printf("\t+------------+------------+-------------------------+\n");
+
+    if( type == ICMP_ECHOREPLY || type == ICMP_ECHO ){
+        printf( "\t| Identification:    %5u| Sequence Number:   %5u|\n", ntohs( icmp->icmp_id ), ntohs( icmp->icmp_seq ) );
+        printf("\t+-------------------------+-------------------------+\n");
+    }
+    else if( type == ICMP_UNREACH ){
+        if( code == ICMP_UNREACH_NEEDFRAG ){
+            printf( "\t| void:          %5u| Next MTU:          %5u|\n", ntohs( icmp->icmp_pmvoid ), ntohs( icmp->icmp_nextmtu ) );
+            printf("\t+-------------------------+-------------------------+\n");
+        }
+        else{
+            printf( "\t| Unused:                                 %10lu|\n", (unsigned long)ntohl( icmp->icmp_void ) );
+            printf("\t+-------------------------+-------------------------+\n");
+        }
+    }
+    else if( type == ICMP_REDIRECT ){
+        printf( "\t| Router IP Address:                 %15s|\n", inet_ntoa( icmp->icmp_gwaddr ) );
+        printf("\t+---------------------------------------------------+\n");
+    }
+    else if( type == ICMP_TIMXCEED ){
+        printf( "\t| Unused:                                 %10lu|\n", (unsigned long)ntohl( icmp->icmp_void ) );
+        printf("\t+---------------------------------------------------+\n");
+    }
+
+
+    if( type == ICMP_UNREACH || type == ICMP_REDIRECT || type == ICMP_TIMXCEED ){
+        struct ip *ip = (struct ip *)icmp->icmp_data;
+        char *p = (char *)ip + ( ip->ip_hl << 2 );
+        dump_ip( (struct ip *)p );    
+        switch( ip->ip_p ){
+            case IPPROTO_TCP:
+                if( type == ICMP_REDIRECT )
+                    dump_tcp_mini( (struct tcphdr *)p );
+                else
+                    dump_tcp( (struct tcphdr *)p );
+                break;
+            case IPPROTO_UDP:
+                dump_udp( (struct udphdr *)p );
+                break;
+        }
+    }
+}
